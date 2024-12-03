08f87e1397436195726ac6eabd06aac9901ae000
jmcoop18 <116752215+jmcoop18@users.noreply.github.com>
2024-12-03 13:42:01 -0500

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,19 @@
+def mult(string):
+    expressions = [i.split(')') for i in string.split('mul(')]
+    t = 0
+    for i in expressions[1:]:
+        try:
+            if (i[0][-1].isnumeric()) and (i[0].count(',') != 0):
+                t += eval(i[0].replace(',', '*'))
+        except:
+            pass
+    return t
+
+f = open('3.txt').read()
+print(mult(f))
+
+f = [i.split("don't()") for i in f.split('do()')]
+p2 = 0
+for i in f:
+    p2 += mult(i[0])
+print(p2)
